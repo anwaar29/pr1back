f3cb956d5fd640a328d3b2436279d61dc9a1852d
anwaar29 <anwaaransari29@gmail.com>
2022-07-08 23:34:50 -0700

    abnhii

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+
 const express = require('express')
 const detalisRouter1 = require('./Routes/details')
 const cors = require("cors")
 const app = express()
+
 app.use(cors())
-app.use("/api",detalisRouter1)
-
-
-app.listen(process.env.PORT||8000,()=>{
+app.use("/api", detalisRouter1)
+app.listen(process.env.PORT || 8000, () => {
     console.log("app is running");
 })
